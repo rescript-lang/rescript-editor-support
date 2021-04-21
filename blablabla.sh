6dcd3a5363fe18327bc85a56a1178fc98456403b
Cheng Lou <chenglou92@gmail.com>
2021-04-21 03:15:16 -0700

    Try a different blabla

--- blablabla.sh
+++ blablabla.sh
@@ -1,17 +1,7 @@
-#!/bin/bash
+echo "hello"
 
 function exp {
-  echo "$(dirname $1)/expected/$(basename $1).txt"
+  echo "bye"
 }
 
-for file in tests/src/*.res; do
-  lib/rescript-editor-support.exe test $file &> $(exp $file)
-done
-
-diff=$(git ls-files --modified tests/src/expected)
-if [[ $diff = "" ]]; then
-  printf "${successGreen}✅ No unstaged tests difference.${reset}\n"
-else
-  printf "${warningYellow}⚠️ There are unstaged differences in tests/! Did you break a test?\n${diff}\n${reset}"
-  exit 1
-fi
+exp "1"
