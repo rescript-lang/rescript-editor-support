a177e976e9080a37982780fd311953c6675cdc5c
Cheng Lou <chenglou92@gmail.com>
2021-04-21 01:28:33 -0700

    Proper makefile test setup
    This now:
    - Automatically installs the tests/ npm dependencies if not found.
    - Automatically builds the tests artifacts if not found.
    - Run the tests through `make test`. No more one-off npm script.
    - Does all that incrementally to avoid hassle.

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,7 @@
-#!/bin/zsh
-
 function exp {
   echo "$(dirname $1)/expected/$(basename $1).txt"
 }
 
-./node_modules/.bin/rescript
-for file in src/**/*.res; do
-  ../lib/rescript-editor-support.exe test $file &> $(exp $file)
+for file in tests/src/*.res; do
+  lib/rescript-editor-support.exe test $file &> $(exp $file)
 done
