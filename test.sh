1f2c85bccdb14d0c901e6cb080ccb0025a9a8f1d
Cheng Lou <chenglou92@gmail.com>
2021-04-21 02:33:58 -0700

    Only diff tests/src/expected

--- test.sh
+++ test.sh
@@ -6,11 +6,10 @@ for file in tests/src/*.res; do
   lib/rescript-editor-support.exe test $file &> $(exp $file)
 done
 
-diff=$(git ls-files --modified tests/)
+diff=$(git ls-files --modified tests/src/expected)
 if [[ $diff = "" ]]; then
   printf "${successGreen}✅ No unstaged tests difference.${reset}\n"
 else
   printf "${warningYellow}⚠️ There are unstaged differences in tests/! Did you break a test?\n${diff}\n${reset}"
-  git diff
   exit 1
 fi
