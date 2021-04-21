4c08f477c516edea6a5625158d1cd59c0a6c118f
Cheng Lou <chenglou92@gmail.com>
2021-04-21 02:26:35 -0700

    Show git diff

--- test.sh
+++ test.sh
@@ -11,5 +11,6 @@ if [[ $diff = "" ]]; then
   printf "${successGreen}✅ No unstaged tests difference.${reset}\n"
 else
   printf "${warningYellow}⚠️ There are unstaged differences in tests/! Did you break a test?\n${diff}\n${reset}"
+  git diff
   exit 1
 fi
