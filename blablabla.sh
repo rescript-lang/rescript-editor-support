f38637dc4ee958d7703a48dee673d769017d8704
Cheng Lou <chenglou92@gmail.com>
2021-04-21 03:09:43 -0700

    Try copy paste makefile to blabla

--- blablabla.sh
+++ blablabla.sh
@@ -1,2 +1,17 @@
-echo "hello"
-echo "goodbye"
+#!/bin/bash
+
+function exp {
+  echo "$(dirname $1)/expected/$(basename $1).txt"
+}
+
+for file in tests/src/*.res; do
+  lib/rescript-editor-support.exe test $file &> $(exp $file)
+done
+
+diff=$(git ls-files --modified tests/src/expected)
+if [[ $diff = "" ]]; then
+  printf "${successGreen}✅ No unstaged tests difference.${reset}\n"
+else
+  printf "${warningYellow}⚠️ There are unstaged differences in tests/! Did you break a test?\n${diff}\n${reset}"
+  exit 1
+fi
